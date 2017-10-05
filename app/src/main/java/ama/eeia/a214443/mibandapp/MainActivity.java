80ba8c285f587a47a7ca192ed3524504c5b8ef10
Adam Klemba <avenn77@wp.pl>
2017-10-05 15:18:37 +0200

    Add library and vibrate method

--- app/src/main/java/ama/eeia/a214443/mibandapp/MainActivity.java
+++ app/src/main/java/ama/eeia/a214443/mibandapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 
 import com.betomaluje.miband.ActionCallback;
 import com.betomaluje.miband.MiBand;
+import com.betomaluje.miband.model.VibrationMode;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +17,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setMiBand();
+        connectMiBand();
+        vibrate();
+    }
+
+    void setMiBand() {
+        miBand = MiBand.getInstance(MainActivity.this);
+    }
+
+    void connectMiBand() {
+        if (!miBand.isConnected()) {
+            miBand.connect(new ActionCallback() {
+                @Override
+                public void onSuccess(Object data) {
+                    Log.d(TAG, "Connected with Mi Band!");
+                    //show SnackBar/Toast or something
+                }
+
+                @Override
+                public void onFail(int errorCode, String msg) {
+                    Log.d(TAG, "Connection failed: " + msg);
+                }
+            });
+        } else {
+            miBand.disconnect();
+        }
+    }
+
+    void vibrate() {
+        miBand.startVibration(VibrationMode.VIBRATION_WITH_LED);
     }
 }
