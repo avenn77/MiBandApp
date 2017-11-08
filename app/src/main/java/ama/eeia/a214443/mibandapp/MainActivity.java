d6c21a79d736d14115300d3f0346d30c85f1c5cd
kapjl-12 <avenn77@wp.pl>
2017-11-08 21:55:45 +0100

    Change used api for MI band 2

--- app/src/main/java/ama/eeia/a214443/mibandapp/MainActivity.java
+++ app/src/main/java/ama/eeia/a214443/mibandapp/MainActivity.java
@@ -1,51 +1,222 @@
 package ama.eeia.a214443.mibandapp;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothProfile;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.betomaluje.miband.ActionCallback;
-import com.betomaluje.miband.MiBand;
-import com.betomaluje.miband.model.VibrationMode;
+
+import java.util.Arrays;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MyMiBandApp";
-    private MiBand miBand;
+    Boolean isListeningHeartRate = false;
 
+    BluetoothAdapter bluetoothAdapter;
+    BluetoothGatt bluetoothGatt;
+    BluetoothDevice bluetoothDevice;
+
+    Button btnStartConnecting, btnGetBoundedDevice, btnStartVibrate, btnStopVibrate;
+    EditText txtPhysicalAddress;
+    TextView txtState, txtByte;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setMiBand();
-        connectMiBand();
-        vibrate();
-    }
 
-    void setMiBand() {
-        miBand = MiBand.getInstance(MainActivity.this);
-    }
+        initializeObjects();
+        initilaizeComponents();
+        initializeEvents();
 
-    void connectMiBand() {
-        if (!miBand.isConnected()) {
-            miBand.connect(new ActionCallback() {
-                @Override
-                public void onSuccess(Object data) {
-                    Log.d(TAG, "Connected with Mi Band!");
-                    //show SnackBar/Toast or something
-                }
+        getBoundedDevice();
+}
 
-                @Override
-                public void onFail(int errorCode, String msg) {
-                    Log.d(TAG, "Connection failed: " + msg);
-                }
-            });
-        } else {
-            miBand.disconnect();
+    void getBoundedDevice() {
+        Set<BluetoothDevice> boundedDevice = bluetoothAdapter.getBondedDevices();
+        for (BluetoothDevice bd : boundedDevice) {
+            if (bd.getName().contains("MI Band 2")) {
+                txtPhysicalAddress.setText(bd.getAddress());
+            }
         }
     }
 
-    void vibrate() {
-        miBand.startVibration(VibrationMode.VIBRATION_WITH_LED);
+    void initializeObjects() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     }
+
+    void initilaizeComponents() {
+        btnStartConnecting = (Button) findViewById(R.id.btnStartConnecting);
+        btnGetBoundedDevice = (Button) findViewById(R.id.btnGetBoundedDevice);
+        btnStartVibrate = (Button) findViewById(R.id.btnStartVibrate);
+        btnStopVibrate = (Button) findViewById(R.id.btnStopVibrate);
+        txtPhysicalAddress = (EditText) findViewById(R.id.txtPhysicalAddress);
+        txtState = (TextView) findViewById(R.id.txtState);
+        txtByte = (TextView) findViewById(R.id.txtByte);
+    }
+
+    void initializeEvents() {
+        btnStartConnecting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startConnecting();
+            }
+        });
+        btnStartVibrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startVibrate();
+            }
+        });
+        btnStopVibrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopVibrate();
+            }
+        });
+        btnGetBoundedDevice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getBoundedDevice();
+            }
+        });
+    }
+
+    void startConnecting() {
+
+        String address = txtPhysicalAddress.getText().toString();
+        bluetoothDevice = bluetoothAdapter.getRemoteDevice(address);
+
+        Log.v("test", "Connecting to " + address);
+        Log.v("test", "Device name " + bluetoothDevice.getName());
+
+        bluetoothGatt = bluetoothDevice.connectGatt(this, true, bluetoothGattCallback);
+
+    }
+
+    void stateConnected() {
+        bluetoothGatt.discoverServices();
+        txtState.setText("Connected");
+    }
+
+    void stateDisconnected() {
+        bluetoothGatt.disconnect();
+        txtState.setText("Disconnected");
+    }
+
+    void listenHeartRate() {
+        BluetoothGattCharacteristic bchar = bluetoothGatt.getService(CustomBluetoothProfile.HeartRate.service)
+                .getCharacteristic(CustomBluetoothProfile.HeartRate.measurementCharacteristic);
+        bluetoothGatt.setCharacteristicNotification(bchar, true);
+        BluetoothGattDescriptor descriptor = bchar.getDescriptor(CustomBluetoothProfile.HeartRate.descriptor);
+        descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+        bluetoothGatt.writeDescriptor(descriptor);
+        isListeningHeartRate = true;
+    }
+
+    void startVibrate() {
+        BluetoothGattCharacteristic bchar = bluetoothGatt.getService(CustomBluetoothProfile.AlertNotification.service)
+                .getCharacteristic(CustomBluetoothProfile.AlertNotification.alertCharacteristic);
+        bchar.setValue(new byte[]{2});
+        if (!bluetoothGatt.writeCharacteristic(bchar)) {
+            Toast.makeText(this, "Failed start vibrate", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void stopVibrate() {
+        BluetoothGattCharacteristic bchar = bluetoothGatt.getService(CustomBluetoothProfile.AlertNotification.service)
+                .getCharacteristic(CustomBluetoothProfile.AlertNotification.alertCharacteristic);
+        bchar.setValue(new byte[]{0});
+        if (!bluetoothGatt.writeCharacteristic(bchar)) {
+            Toast.makeText(this, "Failed stop vibrate", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    final BluetoothGattCallback bluetoothGattCallback = new BluetoothGattCallback() {
+
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            super.onConnectionStateChange(gatt, status, newState);
+            Log.v("test", "onConnectionStateChange");
+
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                stateConnected();
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                stateDisconnected();
+            }
+
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            super.onServicesDiscovered(gatt, status);
+            Log.v("test", "onServicesDiscovered");
+            listenHeartRate();
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicRead(gatt, characteristic, status);
+            Log.v("test", "onCharacteristicRead");
+            byte[] data = characteristic.getValue();
+            txtByte.setText(Arrays.toString(data));
+        }
+
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicWrite(gatt, characteristic, status);
+            Log.v("test", "onCharacteristicWrite");
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            super.onCharacteristicChanged(gatt, characteristic);
+            Log.v("test", "onCharacteristicChanged");
+            byte[] data = characteristic.getValue();
+            txtByte.setText(Arrays.toString(data));
+        }
+
+        @Override
+        public void onDescriptorRead(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+            super.onDescriptorRead(gatt, descriptor, status);
+            Log.v("test", "onDescriptorRead");
+        }
+
+        @Override
+        public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+            super.onDescriptorWrite(gatt, descriptor, status);
+            Log.v("test", "onDescriptorWrite");
+        }
+
+        @Override
+        public void onReliableWriteCompleted(BluetoothGatt gatt, int status) {
+            super.onReliableWriteCompleted(gatt, status);
+            Log.v("test", "onReliableWriteCompleted");
+        }
+
+        @Override
+        public void onReadRemoteRssi(BluetoothGatt gatt, int rssi, int status) {
+            super.onReadRemoteRssi(gatt, rssi, status);
+            Log.v("test", "onReadRemoteRssi");
+        }
+
+        @Override
+        public void onMtuChanged(BluetoothGatt gatt, int mtu, int status) {
+            super.onMtuChanged(gatt, mtu, status);
+            Log.v("test", "onMtuChanged");
+        }
+
+    };
 }
