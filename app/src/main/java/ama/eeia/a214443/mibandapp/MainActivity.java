c6caf461420fd2b446f3593d7e59702e97ed6f0c
kapjl-12 <avenn77@wp.pl>
2017-11-08 23:18:54 +0100

    Find nearby places by network localization

--- app/src/main/java/ama/eeia/a214443/mibandapp/MainActivity.java
+++ app/src/main/java/ama/eeia/a214443/mibandapp/MainActivity.java
@@ -1,5 +1,6 @@
 package ama.eeia.a214443.mibandapp;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
@@ -7,6 +8,11 @@ import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothProfile;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,11 +22,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class MainActivity extends AppCompatActivity {
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     private static final String TAG = "MyMiBandApp";
     Boolean isListeningHeartRate = false;
@@ -32,17 +45,75 @@ public class MainActivity extends AppCompatActivity {
     Button btnStartConnecting, btnGetBoundedDevice, btnStartVibrate, btnStopVibrate;
     EditText txtPhysicalAddress;
     TextView txtState, txtByte;
+    private OkHttpClient client;
+    ExecutorService executor = Executors.newFixedThreadPool(1);
+    private LocationManager locationManager;
+    private double longitude;
+    private double latitude;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initializeObjects();
-        initilaizeComponents();
+        initializeComponents();
         initializeEvents();
 
         getBoundedDevice();
-}
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 2000, 1, this);
+        txtByte.setText("Parapet");
+
+    }
+
+    void checkNow() {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                checkNearbyPlaces();
+            }
+        });
+    }
+
+    void checkNearbyPlaces() {
+        client = new OkHttpClient();
+        String postResponse = null;
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("https")
+                .host("maps.googleapis.com")
+                .addPathSegment("maps")
+                .addPathSegment("api")
+                .addPathSegment("place")
+                .addPathSegment("nearbysearch")
+                .addPathSegment("json")
+                .addQueryParameter("location", latitude + "," + longitude)
+                .addQueryParameter("radius", "100")
+                .addQueryParameter("type", "grocery_or_supermarket")
+                .addQueryParameter("key", "AIzaSyC4l3FOqCAUPYIXEfypto0ceXVZt-qR4rI")
+                .build();
+        System.out.println(url.toString());
+
+        try {
+            postResponse = doGetRequest(url.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(postResponse);
+    }
+
+    String doGetRequest(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
 
     void getBoundedDevice() {
         Set<BluetoothDevice> boundedDevice = bluetoothAdapter.getBondedDevices();
@@ -57,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 
-    void initilaizeComponents() {
+    void initializeComponents() {
         btnStartConnecting = (Button) findViewById(R.id.btnStartConnecting);
         btnGetBoundedDevice = (Button) findViewById(R.id.btnGetBoundedDevice);
         btnStartVibrate = (Button) findViewById(R.id.btnStartVibrate);
@@ -219,4 +290,28 @@ public class MainActivity extends AppCompatActivity {
         }
 
     };
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "LocationChanged");
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        txtByte.setText(latitude + " " + longitude);
+        checkNow();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+        Log.d(TAG, "StatusChanged");
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        Log.d(TAG, "ProviderEnabled");
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Log.d(TAG, "ProviderDisabled");
+    }
 }
